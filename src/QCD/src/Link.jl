c5077f6b9216e0b2d6f0a9a920096720f8ef3db1
Nicholas Pini <ultrapoci@gmail.com>
2022-04-05 12:01:28 +0200

    Link now contains an Sp2Element

--- src/QCD/src/Link.jl
+++ src/QCD/src/Link.jl
@@ -5,14 +5,14 @@ export Link
 struct Link{D}
 	position::NTuple{D, Integer}
 	direction::Integer
-	m::Matrix{Complex} # TODO: see if there's a better way to store Sp(2) in memory
+	m::Sp2Element
 
 	# TODO: cold and hot start
 	function Link(position::NTuple{D, Integer}, direction::Integer) where D
 		if direction > D 
 			throw(ArgumentError("direction = $direction of given link is bigger than dimensions = $D of the lattice"))
 		end
-		m = Matrix{Complex}(I, 4, 4) # TODO: see if Hermitian(m) is useful 
+		m = Sp2Element() 
 		new{D}(position, direction, m)
 	end            
 end
