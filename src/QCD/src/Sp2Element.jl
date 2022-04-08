c918c8ccdecd5fdbe39cd4d476e233569cae1a2e
Nicholas Pini <ultrapoci@gmail.com>
2022-04-08 16:08:03 +0200

    Default to Matrix{ComplexF64}
    instead of generic Number

--- src/QCD/src/Sp2Element.jl
+++ src/QCD/src/Sp2Element.jl
@@ -1,26 +1,38 @@
 export Sp2Element, asmatrix
 
 # TODO check if it is also an element of SU(4)
-struct Sp2Element <: AbstractArray{Number, 2}
-	topleft::Matrix{<:Number}
-	topright::Matrix{<:Number}
+struct Sp2Element <: AbstractMatrix{ComplexF64}
+	topleft::Matrix{ComplexF64}
+	topright::Matrix{ComplexF64}
 
 	function Sp2Element()
-		new(Matrix{Complex{Int}}(I, 2, 2), zeros(Complex{Int}, 2, 2))
+		new(Matrix{ComplexF64}(I, 2, 2), zeros(ComplexF64, 2, 2))
+	end
+
+	function Sp2Element(W::Matrix{ComplexF64}, X::Matrix{ComplexF64})
+		if size(W) ≠ (2, 2) || size(X) ≠ (2, 2)
+			throw(ArgumentError(
+					"One or both of the given matrices don't respect the required dimension of (2,2):\n\
+					\tsize(top left) = $(size(W)),\n\
+					\tsize(top right) = $(size(X))."
+			))	
+		end
+
+		new(W, X)
 	end
 
 	function Sp2Element(W::Matrix{<:Number}, X::Matrix{<:Number})
 		if size(W) ≠ (2, 2) || size(X) ≠ (2, 2)
-			throw(
-				ArgumentError(
+			throw(ArgumentError(
 					"One or both of the given matrices don't respect the required dimension of (2,2):\n\
-					\tsize(W) = $(size(W)),\n\
-					\tsize(X) = $(size(X))."
-				)
-			)	
+					\tsize(top left) = $(size(W)),\n\
+					\tsize(top right) = $(size(X))."
+			))	
 		end
-		new(W, X)
+		new(ComplexF64.(W), ComplexF64.(X))
 	end
+end
+
 end
 
 function asmatrix(S::Sp2Element)
