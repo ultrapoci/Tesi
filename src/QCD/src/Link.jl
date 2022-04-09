cc7ce7a6834da13288c966c7d8433f7292550759
Nicholas Pini <ultrapoci@gmail.com>
2022-04-09 11:47:14 +0200

    Constructor with cartesian index

--- src/QCD/src/Link.jl
+++ src/QCD/src/Link.jl
@@ -2,17 +2,38 @@ using LinearAlgebra: I
 
 export Link
 
-struct Link{D}
-	position::NTuple{D, Integer}
+struct Link{D} <: AbstractMatrix{ComplexF64}
+	s::Sp2Element
 	direction::Integer
-	m::Sp2Element
+	position::CartesianIndex{D}
 
 	# TODO: cold and hot start
-	function Link(position::NTuple{D, Integer}, direction::Integer) where D
-		if direction > D 
-			throw(ArgumentError("direction = $direction of given link is bigger than dimensions = $D of the lattice"))
+	function Link(S::Sp2Element, direction::Integer, position::CartesianIndex{D}) where D
+		if abs(direction) > D 
+			throw(ArgumentError("abs(direction) = $(abs(direction)) of link is bigger than dimensions = $D of the lattice"))
 		end
-		m = Sp2Element() 
-		new{D}(position, direction, m)
-	end            
+
+		new{D}(S, direction, position)
+	end
+
+	function Link(S::Sp2Element, direction::Integer, position::Vararg{Integer, D}) where D
+		Link(S, direction, Tuple(position))
+	end
+	
+	function Link(S::Sp2Element, direction::Integer, position::NTuple{D, Integer}) where D
+		Link(S, direction, CartesianIndex(position))
+	end
+	
+	function Link(direction::Integer, position::CartesianIndex{D}) where D
+		Link(Sp2Element(), direction, position)
+	end
+
+	function Link(direction::Integer, position::NTuple{D, Integer}) where D
+		Link(Sp2Element(), direction, CartesianIndex(position))
+	end
+
+	function Link(direction::Integer, position::Vararg{Integer, D}) where D
+		Link(Sp2Element(), direction, CartesianIndex(position))
+	end
+end
 end
