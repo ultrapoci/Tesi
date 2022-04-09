b639e4d51c18d475588979dfdbf17ca5ce5d54da
Nicholas Pini <ultrapoci@gmail.com>
2022-04-09 11:47:57 +0200

    `unittuple`

--- src/QCD/src/QCD.jl
+++ src/QCD/src/QCD.jl
@@ -1,5 +1,29 @@
 module QCD
 
+export unittuple, directionindex, getlink
+
+function unittuple(::Val{dimensions}, direction::Integer) where dimensions
+	ntuple(
+		i -> i == direction ? 1 : 0,
+		Val(dimensions)
+	)
+end
+
+#* ===== directionindex =====
+"""
+Returns a `CartesianIndex` containing all zeros except a one in the `direction` position. Basically, it returns the unit
+vector in the given direction as a CartesianIndex. Use the version with `Val{dimensions}` or `dimensions` when possible: it is much quicker.
+"""
+function directionindex(::Val{dimensions}, direction::Integer)::CartesianIndex where dimensions
+	CartesianIndex(
+		unittuple(Val(dimensions), direction)
+	)
+end
+
+function directionindex(dimensions::Integer, direction::Integer)::CartesianIndex
+	directionindex(Val(dimensions), direction)
+end
+
 include("Sp2Element.jl")
 include("Link.jl")
 include("Lattice.jl")
