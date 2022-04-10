392e18d2191a1ff3a304e48c0a3210a87755322a
Nicholas Pini <ultrapoci@gmail.com>
2022-04-10 12:04:15 +0200

    subrepresentations() uses SU2Element

--- scripts/sp2.jl
+++ scripts/sp2.jl
@@ -1,11 +1,5 @@
 using LinearAlgebra, QCD
 
-function SU2matrix(t₁::Complex, t₂::Complex)
-	Δ = √(abs2(t₁) + abs2(t₂))
-	m = [t₁ t₂; -conj(t₂) conj(t₁)]
-	(m ./ Δ, Δ)
-end
-
 function subrepresentations(s::Sp2Element)
 	S = asmatrix(s)
 	repr = Tuple{Matrix{Complex}, Real}[]
@@ -13,22 +7,22 @@ function subrepresentations(s::Sp2Element)
 	# SU(2) + U(1) + U(1)
 	t₁ = S[1, 1]
 	t₂ = S[1, 3]
-	push!(repr, SU2matrix(t₁, t₂))
+	push!(repr, SU2Element(t₁, t₂) |> normalizeSU2det)
 
 	# SU(2) + U(1) + U(1)
 	t₁ = S[2, 2]
 	t₂ = S[2, 4]
-	push!(repr, SU2matrix(t₁, t₂))
+	push!(repr, SU2Element(t₁, t₂) |> normalizeSU2det)
 
 	# SU(2) + SU(2)
 	t₁ = S[1, 1] + S[2, 2]
 	t₂ = S[1, 4] + S[2, 3]
-	push!(repr, SU2matrix(t₁, t₂))
+	push!(repr, SU2Element(t₁, t₂) |> normalizeSU2det)
 
 	# SU(2) + SU(2)
 	t₁ = S[1, 1] + S[4, 4]
 	t₂ = S[1, 2] - S[4, 3]
-	push!(repr, SU2matrix(t₁, t₂))
+	push!(repr, SU2Element(t₁, t₂) |> normalizeSU2det)
 
 	repr
 end
