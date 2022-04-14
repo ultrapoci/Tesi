1f9c0cecf6a7b62848bb5af950020a2d9f639794
Nicholas Pini <ultrapoci@gmail.com>
2022-04-14 23:49:09 +0200

    Update parameters.jl

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -1,9 +1,9 @@
 params = @dict(
 	dims = (8, 8, 8),
-	lattice_start = :cold,
+	lattice_start = :hot,
 	β = 1.0,
 
-	nterm = 200, # iterations of termalization
+	nterm = 50, # iterations of termalization
 	norm_every = 5, # after how many cycle to normalize lattice
 	nover = 3 # how many cycles of overrelaxation to do
 )
