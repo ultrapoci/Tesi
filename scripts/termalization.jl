87c061663fed20a5e20d8b5ca06c7974fb7d1480
Nicholas Pini <ultrapoci@gmail.com>
2022-04-14 23:49:34 +0200

    test.jl -> termalization.jl

---
+++ scripts/termalization.jl
@@ -0,0 +1,56 @@
+using DrWatson
+@quickactivate "Tesi"
+
+includet(srcdir("CabibboMarinari.jl"))
+
+using QCD, ProgressMeter, Plots
+
+##
+
+function termalization!(L::Lattice, params::Dict)
+	@unpack β, nterm, nover, norm_every = params
+	
+	@showprogress 1 "Termalization" for n in 1:nterm
+		lattice_overrelaxation!(L, nover)
+		# lattice_heatbath!(L, β)
+		if n % norm_every == 0
+			lattice_normalization!(L)
+		end
+	end
+end
+
+function termalization!(L::Lattice, params::Dict, observable::Function, v::Vector{Float64})
+	@unpack β, nterm, nover, norm_every = params
+	
+	@showprogress 1 "Termalization" for n in 1:nterm
+		lattice_overrelaxation!(L, nover)
+		# lattice_heatbath!(L, β)
+		if n % norm_every == 0
+			lattice_normalization!(L)
+		end
+		push!(v, observable(L))
+	end
+end
+
+
+function termalization(params::Dict)
+	@unpack dims, lattice_start = params
+	lattice = Lattice(dims, start = lattice_start)
+	termalization!(lattice, params)
+	lattice
+end
+
+function termalization(params::Dict, observable::Function)
+	@unpack dims, lattice_start = params
+	lattice = Lattice(dims..., start = lattice_start)
+	v = Float64[]
+	termalization!(lattice, params, observable, v)
+	lattice, v
+end
+
+##
+
+include("parameters.jl")
+display(params)
+lattice, v = termalization(params, action);
+plot(v)
