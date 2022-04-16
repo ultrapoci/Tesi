07080ecb436bd5d793a34965256d59c0e26fd811
Nicholas Pini <ultrapoci@gmail.com>
2022-04-17 00:03:37 +0200

    Main loop for saving results

--- scripts/termalization.jl
+++ scripts/termalization.jl
@@ -3,63 +3,107 @@ using DrWatson
 
 includet(srcdir("CabibboMarinari.jl"))
 
-using QCD, ProgressMeter, Plots, Statistics
-
-##
+using QCD, Plots, Statistics, CSV, ProgressMeter
 
 function termalization!(L::Lattice, params::Dict)
-	@unpack β, nterm, nover, norm_every = params
+	@unpack β, nterm, nover, nnorm = params
 	
 	@showprogress 1 "Termalization" for n in 1:nterm
 		lattice_overrelaxation!(L, nover)
 		lattice_heatbath!(L, β)
-		if n % norm_every == 0
+		if n % nnorm == 0
 			lattice_normalization!(L)
 		end
 	end
 end
 
 function termalization!(L::Lattice, params::Dict, observable::Function, v::Vector)
-	@unpack β, nterm, nover, norm_every = params
+	@unpack β, nterm, nover, nnorm = params
 	
 	@showprogress 1 "Termalization" for n in 1:nterm
 		lattice_overrelaxation!(L, nover)
 		lattice_heatbath!(L, β)
-		if n % norm_every == 0
+		if n % nnorm == 0
 			lattice_normalization!(L)
 		end
 		push!(v, observable(L))
 	end
 end
 
+function termalization!(L::Lattice, params::Dict, observables, v)
+	@unpack β, nterm, nover, nnorm = params
+	
+	@showprogress 1 "Termalization" for n in 1:nterm
+	for n in 1:nterm
+		lattice_overrelaxation!(L, nover)
+		lattice_heatbath!(L, β)
+		if n % nnorm == 0
+			lattice_normalization!(L)
+		end
+
+		w = []
+		for obs in observables
+			push!(w, obs(L))
+		end
+		push!(v, w)
+	end
+end
 
 function termalization(params::Dict)
-	@unpack dims, lattice_start = params
+	@unpack dims, latticestart = params
 	lattice = Lattice(dims, start = lattice_start)
 	termalization!(lattice, params)
 	lattice
 end
 
-function termalization(params::Dict, observable::Function)
-	@unpack dims, lattice_start, sp2type = params
-	lattice = Lattice(dims..., type = sp2type, start = lattice_start)
+function termalization(params::Dict, observables)
+	@unpack dims, latticestart, sp2type = params
+	lattice = Lattice(dims..., type = sp2type, start = latticestart)
 	v = []
-	termalization!(lattice, params, observable, v)
-	lattice, v
+	termalization!(lattice, params, observables, v)
+	lattice, reduce(hcat, v)
+end
+
+function incrementalmean(v)
+	res = []
+	for i in 1:length(v)
+		push!(res, mean(v[1:i]))
+	end
+	res
 end
 
 ##
 
 include("parameters.jl")
-display(params)
 
-lattice, v = termalization(params, averageplaquette)
-println("Avg plaq = ", mean(v))
+for params in dict_list(allparams)
+	show(stdout, "text/plain", params)
+	println()
 
-plaqs = []
-for i in 1:length(v)
-	push!(plaqs, mean(v[1:i]))
+	@unpack observables, to_plot = params
+
+	lattice, all_obs = termalization(params, observables)
+	d = copy(params)
+	for (obs, observable) in zip(eachrow(all_obs), observables)
+		obs_mean = incrementalmean(obs)
+		d[Symbol(observable)] = obs_mean[end]
+		obs_name = String(Symbol(observable))
+
+		println("mean $obs_name = $(obs_mean[end])")
+
+		if to_plot
+			plottitle = savename(params, ignores = "to_plot")
+			p = plot(obs, label = obs_name, title = plottitle, titlefontsize = 10)
+			plot!(p, obs_mean, label = "mean $obs_name")
+			plotname = savename(obs_name, params, "png", ignores = "to_plot")		
+			savefig(p, plotsdir(plotname))
+			display(p)
+		end
+	end
+
+	delete!(d, :to_plot)
+	delete!(d, :observables)
+	csvname = savename(params, "csv", ignores = "to_plot")
+	CSV.write(datadir(csvname), d, writeheader = false)
+	println()
 end
-
-plot(v, label = "plaquette")
-plot!(plaqs, label = "average plaquette")
