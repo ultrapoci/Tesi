3c2f706c887517ea83928caaeae8755ebc832d3a
Nicholas Pini <ultrapoci@gmail.com>
2022-04-17 20:28:29 +0200

    Update parameters.jl

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -5,6 +5,10 @@ allparams = @strdict(
 	sp2type = Sp2ElementB,
 
 	nterm = 100, # iterations of termalization
+	nterm = [
+		@onlyif("β" ≤ 3.0, 100), 
+		@onlyif("β" > 3.0, 50)
+	],
 	nnorm = 10, # after how many cycle to normalize lattice
 	nover = 3, # how many cycles of overrelaxation to do
 )
@@ -12,5 +16,5 @@ allparams = @strdict(
 obsparams = @strdict(
 	observables = averageplaquette, # function taking Lattice as input
 	to_plot = true,
-	meanoffset = 1, # from which iteration to being calculating the mean
+	meanoffset = 5, # from which iteration to being calculating the mean
 )
