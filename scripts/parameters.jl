38f0bf563566aac6fc1e98cf665e87acdaf97e20
Nicholas Pini <ultrapoci@gmail.com>
2022-04-20 11:50:29 +0200

    abstract type Params

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -1,40 +1,49 @@
-Base.@kwdef struct ObsParams
-	observables = ("avg_plaq" => averageplaquette, "action" => action)
-	to_plot = true
+abstract type Params <: AbstractDict{String, Any} end
+
+Base.@kwdef struct ObsParams <: Params
+	observables = "avg_plaq" => averageplaquette
+	save_plot = true
+	display_plot = true
 	save_dat = true
 	save_jld2 = true
 	save_df = true
 end
 
-Base.@kwdef struct TermParams <: AbstractDict{String, Any}
+Params(::Type{ObsParams}, x...) = ObsParams(x...) 
+
+Base.@kwdef struct TermParams <: Params
 	dims = [(8, 8, 8), (8, 8, 8, 8)]
 	β = [i/2 for i in 1.0:15.0]
 	latticestart = :cold
 	sp2type = Sp2ElementB
 
-	nterm = 100
+	nterm = 200
 	nnorm = 5 # after how many cycle to normalize lattice
 	nover = 3 # how many cycles of overrelaxation to do
 	
-	meanoffset = 1 # from which iteration to start calculating the mean
+	meanoffset = 50 # from which iteration to start calculating the mean
 end
 
-function Base.iterate(X::TermParams, state)
+Params(::Type{TermParams}, x...) = TermParams(x...) 
+DrWatson.default_allowed(::TermParams) = (Real, String, Tuple)
+DrWatson.allaccess(::TermParams) = (:dims, :β, :nterm, :nover, :nnorm)
+
+function Base.iterate(X::Params, state)
 	if length(state) > 0
 		k = state[begin]
 		return String(k) => getfield(X, k), Base.tail(state)
 	end
 	nothing
 end
-Base.iterate(X::TermParams) = Base.iterate(X, fieldnames(TermParams))
-Base.length(::TermParams) = fieldcount(TermParams)
-Base.get(X::TermParams, s::Symbol, ::Symbol) = Base.getproperty(X, s)
-Base.get(X::TermParams, s, ::Symbol) = Base.getproperty(X, Symbol(s))
+Base.iterate(X::Params) = Base.iterate(X, fieldnames(typeof(X)))
+Base.length(X::Params) = fieldcount(typeof(X))
+Base.get(X::Params, s::Symbol, ::Symbol) = Base.getproperty(X, s)
+Base.get(X::Params, s, ::Symbol) = Base.getproperty(X, Symbol(s))
 
-function DrWatson.dict_list(p::TermParams)
-	[TermParams([d[String(field)] for field in fieldnames(TermParams)]...)
-	for d in DrWatson.dict_list(Dict(p))]
-end
-
-DrWatson.default_allowed(::TermParams) = (Real, String, Tuple)
-DrWatson.allaccess(::TermParams) = (:dims, :β, :nterm, :nover, :nnorm)
+DrWatson.dict_list(p::Params) = [
+	Params(
+		typeof(p), 
+		[d[String(field)] for field in fieldnames(typeof(p))]...
+	)
+	for d in DrWatson.dict_list(Dict(p))
+]
