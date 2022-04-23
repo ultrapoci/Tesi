3be9cfd5943f07a57bdb6718983a83d8358822e4
Nicholas Pini <ultrapoci@gmail.com>
2022-04-24 00:36:02 +0200

    Update parameters.jl

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -3,30 +3,35 @@ abstract type Params <: AbstractDict{String, Any} end
 Base.@kwdef struct ObsParams <: Params
 	observables = "avg_plaq" => averageplaquette
 	save_plot = true
-	display_plot = false
+	display_plot = true
 	save_dat = true
 	save_jld2 = true
 	save_df = true
 end
 
-Params(::Type{ObsParams}, x...) = ObsParams(x...) 
-
 Base.@kwdef struct TermParams <: Params
 	dims = (8, 8, 8)
-	β = [i/2 for i in 1.0:15.0]
+	#β = [i/2 for i in 1.0:15.0]
+	β = 1.0
 	latticestart = :cold
-	sp2type = Sp2ElementB
+	#sp2type = Sp2ElementB
+	sp2type = nothing
 
-	nterm = 200
-	nnorm = 20 # after how many cycle to normalize lattice
+	nterm = 50
+	nnorm = 10 # after how many cycle to normalize lattice
 	nover = 3 # how many cycles of overrelaxation to do
 	
-	meanoffset = 10 # from which iteration to start calculating the mean
+	meanoffset = 5 # from which iteration to start calculating the mean
 
 	nobs = 1 # measure observables every nobs cycles
 end
 
-Params(::Type{TermParams}, x...) = TermParams(x...) 
+
+#* ===== FUNCTIONS =====
+
+Params(::Type{ObsParams}, x...) = ObsParams(x...) 
+Params(::Type{TermParams}, x...) = TermParams(x...)
+
 DrWatson.default_allowed(::TermParams) = (Real, String, Tuple)
 DrWatson.allaccess(::TermParams) = (:dims, :β, :nterm, :nover, :nnorm)
 
