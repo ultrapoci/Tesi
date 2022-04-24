c7767bfcf4f9200ba5c27402c4d47429e028d41c
Nicholas Pini <ultrapoci@gmail.com>
2022-04-24 18:06:27 +0200

    Update parameters.jl

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -1,7 +1,7 @@
 Base.@kwdef struct ObsParams <: Params
 	observables = "avg_plaq" => averageplaquette
 	save_plot = true
-	display_plot = true
+	display_plot = false
 	save_dat = true
 	save_jld2 = true
 	save_df = true
@@ -9,16 +9,15 @@ end
 
 Base.@kwdef struct TermParams <: Params
 	dims = (8, 8, 8)
-	#β = [i/2 for i in 1.0:15.0]
-	β = 1.0
+	β = [i/2 for i in 1.0:15.0]
 	latticestart = :cold
 	#sp2type = Sp2ElementB
 	
-	nterm = 50
+	nterm = 200
 	nnorm = 10 # after how many cycle to normalize lattice
 	nover = 3 # how many cycles of overrelaxation to do
 	
-	meanoffset = 5 # from which iteration to start calculating the mean
+	meanoffset = 10 # from which iteration to start calculating the mean
 
 	nobs = 1 # measure observables every nobs cycles
 end
