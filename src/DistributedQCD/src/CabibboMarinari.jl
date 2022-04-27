b30db0f9fab33002c8c6d7d0dc0367b6ee50d477
Nicholas Pini <ultrapoci@gmail.com>
2022-04-28 00:28:03 +0200

    log with iter

--- src/DistributedQCD/src/CabibboMarinari.jl
+++ src/DistributedQCD/src/CabibboMarinari.jl
@@ -22,12 +22,9 @@ const subrepresentations = [
 Generates a real number ``a₀`` according to the distribution P(a₀) = √(1 - a₀^2) exp(a₀ β k).		
 """
 function generate_a0(k::Real, β::Real)
-	if k < 0
-		throw(ArgumentError("k must be positive, got $k."))
-	elseif β < 0
-		throw(ArgumentError("β must be positive, got $β."))
-	end
-
+	k < 0 && throw(ArgumentError("k must be positive, got $k."))
+	β < 0 && throw(ArgumentError("β must be positive, got $β."))
+	
 	reject = true
 	a₀ = 0.0
 	while reject
@@ -72,7 +69,7 @@ function touch_heatbath(S::Sp2, R::StaticMatrix{4, 4, ComplexF64}, β::Real)
 end
 
 """
-	overrelaxation!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int; log = false) where D
+	overrelaxation!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int; log = false, iter = missing) where D
 	overrelaxation!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, nover::Int; kwargs...) where D
 	overrelaxation!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, β::Real; kwargs...) where D
 Do `nover` iterations of overrelaxation to the lattice `L`. `mask` is a boolean `DArray` with the same dimensions \
@@ -80,9 +77,9 @@ and distributed in the same way of `L`. The same is true for `inds`, only it con
 
 The named tuple returned by the function `newlattice` can be passed directly as the first argument.
 """
-function overrelaxation!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int; log = false) where D
+function overrelaxation!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int; log = false, iter = missing) where D
 	for i in 1:nover, parity in (:even, :odd), u in 1:D
-		log && @info "Overrelaxation" i parity u
+		log && @info "Overrelaxation" iter cycle=i parity direction=u
 		with_workers() do _
 			mask = parity == :even ? evenmask[:L] : .!evenmask[:L]
 
@@ -99,16 +96,16 @@ overrelaxation!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, nover::Int; kwargs...
 overrelaxation!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, nover::Int; kwargs...) where D = overrelaxation!(T..., nover; kwargs...)
 
 """
-	heatbath!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, β::Real; log = false) where D
+	heatbath!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, β::Real; log = false, iter = missing) where D
 	heatbath!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, β::Real; kwargs...) where D 
 	heatbath!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, β::Real; kwargs...) where D
 Same as `overrelaxation`, only it applies the heat-bath algorithm to the lattice `L`, and does only one iteration.
 
 `β` is the coefficient in front of the action in the exponential of the Boltzmann distribution.
 """
-function heatbath!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, β::Real; log = false) where D
+function heatbath!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, β::Real; log = false, iter = missing) where D
 	for parity in (:even, :odd), u in 1:D
-		log && @info "Heat-bath" parity u
+		log && @info "Heat-bath" iter parity direction=u
 		with_workers() do _
 			mask = parity == :even ? evenmask[:L] : .!evenmask[:L]
 
@@ -125,13 +122,13 @@ heatbath!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, β::Real; kwargs...) where 
 heatbath!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, β::Real; kwargs...) where D = heatbath!(T..., β; kwargs...)
 
 """
-	normalizelattice!(L::Lattice{D}; log = false) where D
+	normalizelattice!(L::Lattice{D}; log = false, iter = missing) where D
 	normalizelattice!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
 	normalizelattice!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
 Normalize all links in the lattice `L` to make sure they belong to ``\\mathrm{Sp(2)}`` and ``\\mathrm{SU(4)}``. It is possible to directly pass a tuple or named tuple containing the lattice. 
 """
-function normalizelattice!(L::Lattice{D}; log = false) where D
-	log && @info "Normalizing..."
+function normalizelattice!(L::Lattice{D}; log = false, iter = missing) where D
+	log && @info "Normalizing..." iter
 	with_workers() do _
 		@maybe_threaded for u in 1:D
 			for x in eachindex(L[:L])
@@ -144,15 +141,15 @@ normalizelattice!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D 
 normalizelattice!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = normalizelattice!(T.lattice; kwargs...)
 
 """
-	one_termalization!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int, β::Real, do_normalization = false; log = false) where D
+	one_termalization!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int, β::Real, do_normalization = false; log = false, iter = missing) where D
 	one_termalization!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, nover::Int, β::Real, do_normalization = false; kwargs...) where D
 	one_termalization!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, nover::Int, β::Real, do_normalization = false; kwargs...) where D
 Do one iteration of termalization, meaning `nover` cycles of overrelaxation, one cycle of heatbath and, if `do_normalization` is true, normalize all lattice.
 """
-function one_termalization!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int, β::Real, do_normalization = false; log = false) where D
-	overrelaxation!(L, evenmask, inds, nover, log = log)
-	heatbath!(L, evenmask, inds, β, log = log)
-	do_normalization && normalizelattice!(L, log = log)
+function one_termalization!(L::Lattice{D}, evenmask::Mask{D}, inds::Indices{D}, nover::Int, β::Real, do_normalization = false; log = false, iter = missing) where D
+	overrelaxation!(L, evenmask, inds, nover, log = log, iter = iter)
+	heatbath!(L, evenmask, inds, β, log = log, iter = iter)
+	do_normalization && normalizelattice!(L, log = log, iter = iter)
 end
 one_termalization!(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, nover::Int, β::Real, do_normalization = false; kwargs...) where D = one_termalization!(T..., nover, β, do_normalization; kwargs...)
 one_termalization!(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, nover::Int, β::Real, do_normalization = false; kwargs...) where D = one_termalization!(T..., nover, β, do_normalization; kwargs...)
