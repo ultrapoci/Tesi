d8dbae5ac5ce78b4f7e5d9682d9f73bb784065eb
Nicholas Pini <ultrapoci@gmail.com>
2022-05-01 12:07:33 +0200

    Update parameters.jl

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -1,25 +1,26 @@
 Base.@kwdef struct ObsParams <: Params
-	save_plot = false
-	display_plot = false
-	save_dat = false
-	save_jld2 = false
-	save_df = false
+	save_plot = true
+	display_plot = true
+	save_dat = true
+	save_jld2 = true
+	save_df = true
 	observables = (
-		#"avg_plaq" => averageplaquette,
-		#"mod_polyloop" => expval_modpolyloop,
+		"avg_plaq" => averageplaquette,
+		"mod_polyloop" => expval_modpolyloop,
 		"χ" => susceptibility,
 		"χᵥ" => susceptibility_pervolume,
+		"polyloop squared over volume" => (L; kwargs...) -> sum(DistributedQCD.all_polyloops(L) .^ 2) / 8^3,
 	)
 end
 
 Base.@kwdef struct TermParams <: Params
 	dims = (2, 8, 8, 8)
 	#β = [i/2 for i in 1.0:15.0]
-	β = 6.45 / 2
-	latticestart = :cold
+	β = 6.45
+	latticestart = :hot
 	sp2type = try Sp2ElementB catch; missing end
 	
-	nterm = 100
+	nterm = 50
 	nnorm = 10 # after how many cycle to normalize lattice
 	nover = 3 # how many cycles of overrelaxation to do
 	
