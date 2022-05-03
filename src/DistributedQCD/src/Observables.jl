0a3e45c2cb8b4b6f035a096a886ffdf7de553205
Nicholas Pini <ultrapoci@gmail.com>
2022-05-03 22:42:02 +0200

    observables now take generic namedtuple
    this is done to avoid errors when beta is either passed or not, without having to rely on kwargs

--- src/DistributedQCD/src/Observables.jl
+++ src/DistributedQCD/src/Observables.jl
@@ -1,82 +1,50 @@
-#=
-* Observables accept any kwarg without throwing error, so that they can be called with a common interface.
-=#
-
+#* ===== average plaquette =====
 """
 	averageplaquette(L::Lattice{D}, inds::Indices{D}; log = false, iter = missing) where D
-	averageplaquette(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
-	averageplaquette(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
+	averageplaquette(T::NamedTuple; kwargs...)
 Return the value of the average plaquette of the lattice `L` divided by ``2N``, where ``N`` is the dimensions of ``Sp(N)``. In this case, ``N = 2``.
 """
 function averageplaquette(L::Lattice{D}, inds::Indices{D}; log = false, iter = missing) where D
 	log && @info "Measuring average plaquette..." iter
-
-	current_workers = vec(procs(L))
-	n_current_workers = length(current_workers)
-	# initialize a vector with nworkers() elements, of which each worker owns one cell
-	partial = dzeros((n_current_workers,), current_workers, [n_current_workers])
-	with_workers() do _	
-		tot = 0.0
-		for x in CartesianIndices(L[:L]), u in 1:D-1, v in u+1:D
-			tot += tr(L[:L][x][u] * staple(L, v, u, inds[:L][x]))
-		end
-		partial[:L][begin] = tot
-	end
-	res = sum(partial)
+	res = plaquettesum(L, inds)
 	V = length(L) # lattice's volume
 	np = D * (D - 1) ÷ 2 # number of plaquettes per site
-
-	# the 4 term is due to 2N for N=2
-	res / (4 * np * V) 
+	res / (4 * np * V)  # the 4 term is due to 2N for N=2
 end
-averageplaquette(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = averageplaquette(T[1], T[3]; kwargs...)
-averageplaquette(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = averageplaquette(T.lattice, T.inds; kwargs...)
+averageplaquette(T::NamedTuple; kwargs...) = averageplaquette(T.lattice, T.inds; kwargs...)
 
+
+#* ===== action =====
 """
 	action(L::Lattice{D}, inds::Indices{D}; β, log = false, iter = missing) where D
-	action(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
-	action(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
+	action(T::NamedTuple; kwargs...)
 Return the action for β = 8/g², defined as -2/g² times the sum over all traces of plaquettes.
 """
 function action(L::Lattice{D}, inds::Indices{D}, β::Real; log = false, iter = missing) where D
-	log && @info "Measuring action..." iter
-	
-	current_workers = vec(procs(L))
-	n_current_workers = length(current_workers)
-	# initialize a vector with nworkers() elements, of which each worker owns one cell
-	partial = dzeros((n_current_workers,), current_workers, [n_current_workers])
-	with_workers() do _	
-		tot = 0.0
-		for x in CartesianIndices(L[:L]), u in 1:D-1, v in u+1:D
-			tot += tr(L[:L][x][u] * staple(L, v, u, inds[:L][x])) # trace of plaquette
-		end
-		partial[:L][begin] = tot
-	end
-	res = sum(partial)
-
+	log && @info "Measuring action..." iter	
+	res = plaquettesum(L, inds)
 	-β * res / 4 # 4 is due to trace normalization 
 end
-action(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, β::Real; kwargs...) where D = action(T[1], T[3], β; kwargs...)
-action(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, β::Real; kwargs...) where D = action(T.lattice, T.inds, β; kwargs...)
+action(T::NamedTuple, β::Real; kwargs...) = action(T.lattice, T.inds, β; kwargs...)
+action(T::NamedTuple; kwargs...) = action(T.lattice, T.inds, T.β; kwargs...)
 
 """
 	actionsquared(L::Lattice{D}, inds::Indices{D}, β; log = false, iter = missing) where D
-	actionsquared(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, β; kwargs...) where D
-	actionsquared(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, β; kwargs...) where D
+	actionsquared(T::NamedTuple, β; kwargs...)
 Return the action squared for β = 8/g², defined as -2/g² times the sum over all traces of plaquettes.
 """
 function actionsquared(L::Lattice{D}, inds::Indices{D}, β::Real; log = false, iter = missing) where D
 	log && @info "Measuring action squared..." iter
-
 	action(L, inds, β)^2
 end
-actionsquared(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, β; kwargs...) where D = actionsquared(T[1], T[3], β; kwargs...)
-actionsquared(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, β; kwargs...) where D = actionsquared(T.lattice, T.inds, β; kwargs...)
+actionsquared(T::NamedTuple, β::Real; kwargs...) = actionsquared(T.lattice, T.inds, β; kwargs...)
+actionsquared(T::NamedTuple; kwargs...) = actionsquared(T.lattice, T.inds, T.β; kwargs...)
 
+
+#* ===== susceptibility =====
 """
 	susceptibility(L::Lattice{D}; log = false, iter = missing) where D
-	susceptibility(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
-	susceptibility(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
+	susceptibility(T::NamedTuple; kwargs...)
 Return the susceptibility χ of the lattice `L`, defined as \
 ``\\chi = \\sum_{\\vec{x}} \\left< \\varphi_{\\vec{0}} \\right> \\left< \\varphi_{\\vec{x}} \\right> = L^D \\left< \\varphi^2 \\right>``.
 
@@ -84,11 +52,9 @@ Can be called with the symbol `χ`.
 """
 function susceptibility(L::Lattice{D}; log = false, iter = missing) where D
 	log && @info "Measuring susceptibility..." iter
-
 	sum(all_polyloops(L) .^ 2) / prod(tail(size(L)))
 end
-susceptibility(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = susceptibility(T[1]; kwargs...)
-susceptibility(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = susceptibility(T.lattice; kwargs...)
+susceptibility(T::NamedTuple; kwargs...) = susceptibility(T.lattice; kwargs...)
 
 """
 See `susceptibility`.
@@ -97,29 +63,27 @@ See `susceptibility`.
 
 """
 	susceptibility_pervolume(L::Lattice{D}; log = false, iter = missing) where D
-	susceptibility_pervolume(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
-	susceptibility_pervolume(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
+	susceptibility_pervolume(T::NamedTuple; kwargs...)
 Return the susceptibility χ of the lattice `L` divided by the lattice spatial volume.
 
 Can be called with the symbol `χᵥ`.
 """
 function susceptibility_pervolume(L::Lattice{D}; log = false, iter = missing) where D
 	log && @info "Measuring susceptibility per volume..." iter
-
 	susceptibility(L) / prod(tail(size(L)))
 end
-susceptibility_pervolume(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = susceptibility_pervolume(T[1]; kwargs...)
-susceptibility_pervolume(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = susceptibility_pervolume(T.lattice; kwargs...)
+susceptibility_pervolume(T::NamedTuple; kwargs...) = susceptibility_pervolume(T.lattice; kwargs...)
 
 """
 See `susceptibility_pervolume`.
 """
 χᵥ = susceptibility_pervolume
 
+
+#* ===== Binder cumulant =====
 """
 	binder(L::Lattice{D}; log = false, iter = missing) where D
-	binder(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
-	binder(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D.
+	binder(T::NamedTuple; kwargs...)
 Return the Binder cumulant of the lattice `L`, defined as \
 ``g_{R} = \\frac{\\left< \\varphi^4 \\right>}{\\left< \\varphi^2 \\right>^2} - 3``.
 
@@ -135,18 +99,18 @@ function binder(L::Lattice{D}; log = false, iter = missing) where D
 	
 	Vₛ * φ⁴ / (φ²^2) - 3
 end
-binder(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = binder(T[1]; kwargs...)
-binder(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = binder(T.lattice; kwargs...)
+binder(T::NamedTuple; kwargs...) = binder(T.lattice; kwargs...)
 
 """
 See `binder`.
 """
 gᵣ = binder
 
+
+#* ===== exp. val. of Polyakov loop =====
 """
 	expval_polyloop(L::Lattice{D}; log = false, iter = missing) where D
-	expval_polyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
-	expval_polyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
+	expval_polyloop(T::NamedTuple; kwargs...)
 Return the expectation value of the Polyakov loop of the lattice `L`.
 """
 function expval_polyloop(L::Lattice{D}; log = false, iter = missing) where D
@@ -157,13 +121,11 @@ function expval_polyloop(L::Lattice{D}; log = false, iter = missing) where D
 
 	res / Vₛ
 end
-expval_polyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = expval_polyloop(T[1]; kwargs...)
-expval_polyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = expval_polyloop(T.lattice; kwargs...)
+expval_polyloop(T::NamedTuple; kwargs...) = expval_polyloop(T.lattice; kwargs...)
 
 """
 	expval_modpolyloop(L::Lattice{D}; log = false, iter = missing) where D
-	expval_modpolyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D
-	expval_modpolyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
+	expval_modpolyloop(T::NamedTuple; kwargs...)
 Return the expectation value of the modulus the Polyakov loop of the lattice `L`.
 """
 function expval_modpolyloop(L::Lattice{D}; log = false, iter = missing) where D
@@ -174,15 +136,15 @@ function expval_modpolyloop(L::Lattice{D}; log = false, iter = missing) where D
 
 	res / Vₛ
 end
-expval_modpolyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = expval_modpolyloop(T[1]; kwargs...)
-expval_modpolyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = expval_modpolyloop(T.lattice; kwargs...)
+expval_modpolyloop(T::NamedTuple; kwargs...) = expval_modpolyloop(T.lattice; kwargs...)
 
+
+#* ===== Polyakov loops =====
 """
 	polyloop(L::Lattice{D}, x::NTuple{Dm1, Int}; log = false, iter = missing) where {D, Dm1}
 	polyloop(L, x; kwargs...) = polyloop(L, Tuple(x); kwargs...)
 	polyloop(L, x...; kwargs...) = polyloop(L, Tuple(x); kwargs...)
-	polyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x; kwargs...) where D = polyloop(T[1], x; kwargs...)
-	polyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, x; kwargs...) where D = polyloop(T.lattice, x; kwargs...)
+	polyloop(T::NamedTuple, x; kwargs...)
 Return the Polyakov loop calculated at spatial point `x` of the lattice `L`. `x` must be compatible with `L`'s spatial dimensions.
 """
 function polyloop(L::Lattice{D}, inds::Indices{D}, x::NTuple{Dm1, Int}; log = false, iter = missing) where {D, Dm1}
@@ -213,14 +175,12 @@ function polyloop(L::Lattice{D}, inds::Indices{D}, x::NTuple{Dm1, Int}; log = fa
 end
 polyloop(L, inds, x; kwargs...) = polyloop(L, inds, Tuple(x); kwargs...)
 polyloop(L, inds, x...; kwargs...) = polyloop(L, inds, Tuple(x); kwargs...)
-polyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x...; kwargs...) where D = polyloop(T[1], T[3], x...; kwargs...)
-polyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, x...; kwargs...) where D = polyloop(T.lattice, T.inds, x...; kwargs...)
+polyloop(T::NamedTuple, x...; kwargs...) = polyloop(T.lattice, T.inds, x...; kwargs...)
 
 """
 	corr_polyloop(L::Lattice{D}, x::NTuple{Dm1, Int}, y::NTuple{Dm1, Int}; log = false, iter = missing) where {D, Dm1}
-	corr_polyloop(L, x, y; kwargs...) = corr_polyloop(L, Tuple(x), Tuple(y); kwargs...)
-	corr_polyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x, y; kwargs...) where D = corr_polyloop(T[1], x, y; kwargs...)
-	corr_polyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, x, y; kwargs...) where D = corr_polyloop(T.lattice, x, y; kwargs...)
+	corr_polyloop(L, x, y; kwargs...)
+	corr_polyloop(T::NamedTuple, x, y; kwargs...)
 Return the two point correlation function of the Polyakov loops at points `x` and `y`, which must be compatible with the spatial dimensions of the lattice `L`.
 """
 function corr_polyloop(L::Lattice{D}, inds::Indices{D}, x::NTuple{Dm1, Int}, y::NTuple{Dm1, Int}; log = false, iter = missing) where {D, Dm1}
@@ -263,13 +223,34 @@ function corr_polyloop(L::Lattice{D}, inds::Indices{D}, x::NTuple{Dm1, Int}, y::
 	trX * trY
 end
 corr_polyloop(L, inds, x, y; kwargs...) = corr_polyloop(L, inds, Tuple(x), Tuple(y); kwargs...)
-corr_polyloop(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x, y; kwargs...) where D = corr_polyloop(T[1], T[3], x, y; kwargs...)
-corr_polyloop(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, x, y; kwargs...) where D = corr_polyloop(T.lattice, T.inds, x, y; kwargs...)
+corr_polyloop(T::NamedTuple, x, y; kwargs...) = corr_polyloop(T.lattice, T.inds, x, y; kwargs...)
+
+
+#* ===== intermediate useful functions =====
+"""
+	plaquettesum(L::Lattice{D}, inds::Indices{D}) where D
+	plaquettesum(T::NamedTuple; kwargs...)
+Return the sum overof the traces of all plaquettes. 
+"""
+function plaquettesum(L::Lattice{D}, inds::Indices{D}) where D
+	current_workers = vec(procs(L))
+	n_current_workers = length(current_workers)
+	# initialize a vector with nworkers() elements, of which each worker owns one cell
+	partial = dzeros((n_current_workers,), current_workers, [n_current_workers])
+	with_workers() do _	
+		tot = 0.0
+		for x in CartesianIndices(L[:L]), u in 1:D-1, v in u+1:D
+			tot += tr(L[:L][x][u] * staple(L, v, u, inds[:L][x]))
+		end
+		partial[:L][begin] = tot
+	end
+	sum(partial)
+end
+plaquettesum(T::NamedTuple; kwargs...) = plaquettesum(T.lattice, T.inds; kwargs...)
 
 """
 	all_polyloops(L::Lattice{D}) where D
-	all_polyloops(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = all_polyloops(T[1]; kwargs...)
-	all_polyloops(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D
+	all_polyloops(T::NamedTuple; kwargs...)
 Return an array with the same spatial dimension of the lattice `L`. Every cell contains the trace of the Polyakov loop at that spatial point.
 """
 function all_polyloops(L::Lattice{D}) where D
@@ -296,8 +277,7 @@ function all_polyloops(L::Lattice{D}) where D
 	# "mortar" transforms the array of arrays into a single array that corresponds to the spatial slice of L
 	tr.(convert(Array, mortar([reduce(.*, loc[:, x]) for x in CartesianIndices(spacedist)])))
 end
-all_polyloops(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}; kwargs...) where D = all_polyloops(T[1]; kwargs...)
-all_polyloops(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}; kwargs...) where D = all_polyloops(T.lattice; kwargs...)
+all_polyloops(T::NamedTuple; kwargs...) = all_polyloops(T.lattice; kwargs...)
 
 
 
@@ -312,8 +292,8 @@ all_polyloops(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D},
 end
 polyloop2(L, x; kwargs...) = polyloop2(L, Tuple(x); kwargs...)
 polyloop2(L, x...; kwargs...) = polyloop2(L, Tuple(x); kwargs...)
-polyloop2(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x; kwargs...) where D = polyloop2(T[1], x; kwargs...)
-polyloop2(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, x; kwargs...) where D = polyloop2(T.lattice, x; kwargs...) =#
+polyloop2(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x; kwargs...) = polyloop2(T[1], x; kwargs...)
+polyloop2(T::NamedTuple, x; kwargs...) = polyloop2(T.lattice, x; kwargs...) =#
 
 #= function corr_polyloop2(L::Lattice{D}, x::NTuple{Dm1, Int}, y::NTuple{Dm1, Int}; log = false, iter = missing) where {D, Dm1}
 	Dm1 ≠ D-1 && throw(TypeError(polyloop, NTuple{D-1, Int}, NTuple{Dm1, Int}))
@@ -324,8 +304,8 @@ polyloop2(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Ind
 	loops[x...] * loops[y...]
 end
 corr_polyloop2(L, x, y; kwargs...) = corr_polyloop2(L, Tuple(x), Tuple(y); kwargs...)
-corr_polyloop2(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x, y; kwargs...) where D = corr_polyloop2(T[1], x, y; kwargs...)
-corr_polyloop2(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, x, y; kwargs...) where D = corr_polyloop2(T.lattice, x, y; kwargs...) =#
+corr_polyloop2(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x, y; kwargs...) = corr_polyloop2(T[1], x, y; kwargs...)
+corr_polyloop2(T::NamedTuple, x, y; kwargs...) = corr_polyloop2(T.lattice, x, y; kwargs...) =#
 
 #= function corr_polyloop3(L::Lattice{D}, x::NTuple{Dm1, Int}, y::NTuple{Dm1, Int}; log = false) where {D, Dm1}
 	Dm1 ≠ D-1 && throw(TypeError(corr_loop, CartesianIndex{D-1}, CartesianIndex{Dm1}))
@@ -335,5 +315,5 @@ corr_polyloop2(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}
 	polyloop2(L, x) * polyloop2(L, y)
 end
 corr_polyloop3(L, x, y; kwargs...) = corr_polyloop3(L, Tuple(x), Tuple(y); kwargs...)
-corr_polyloop3(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x, y; kwargs...) where D = corr_polyloop3(T[1], x, y; kwargs...)
-corr_polyloop3(T::NamedTuple{(:lattice, :mask, :inds), Tuple{Lattice{D}, Mask{D}, Indices{D}}}, x, y; kwargs...) where D = corr_polyloop3(T.lattice, x, y; kwargs...) =#
+corr_polyloop3(T::Tuple{Lattice{D}, Mask{D}, Indices{D}}, x, y; kwargs...) = corr_polyloop3(T[1], x, y; kwargs...)
+corr_polyloop3(T::NamedTuple, x, y; kwargs...) = corr_polyloop3(T.lattice, x, y; kwargs...) =#
