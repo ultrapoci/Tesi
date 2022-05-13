da7a9af43c2b836e54e2ab69dcc00dae200910a2
Nicholas Pini <ultrapoci@gmail.com>
2022-05-13 14:52:28 +0200

    Update termalization.jl

--- notebooks/termalization.jl
+++ notebooks/termalization.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.3
+# v0.19.4
 
 using Markdown
 using InteractiveUtils
@@ -22,65 +22,125 @@ using DrWatson
 @quickactivate "Tesi"
 
 # ╔═╡ 0aafddce-0c54-4df5-bf3e-87d684782c87
-using DistributedQCD, Plots, DataFrames, DataFramesMeta, PlutoUI
+using DistributedQCD, Plots, DataFrames, DataFramesMeta, PlutoUI, Statistics, Measurements, LaTeXStrings
 
 # ╔═╡ d5095d25-a5ed-432f-b5dc-8e5f2c68f35f
 md"""
 # Plots
 """
 
-# ╔═╡ bb4b3a80-9310-44b1-ad13-2562fc6502bc
+# ╔═╡ 47761aa2-7222-444b-934d-e0593b2cecbd
 md"""
-Observable to plot:
-$(@bind col Select([
-	:polyloop => "exp. val. of Polyakov loops", 
-	:mod_polyloop => "exp. val. of modulus of Polyakov loops",
-	:avg_plaq => "average plaquette",
-]))
+## Data manipulation
+"""
+
+# ╔═╡ 4ccc646e-0602-4d6b-a0bf-d412261f9598
+md"""
+### Sort by β and dims
+"""
+
+# ╔═╡ ad47f215-ba50-45d6-b105-c584e30c85cc
+md"""
+### Remove duplicates
+"""
+
+# ╔═╡ e2d78392-435c-4001-8596-f42da7858e07
+md"""
+### Subtract the condensate from χ/L³
+"""
+
+# ╔═╡ dff10576-2feb-418c-903e-ef11e47f8d74
+md"""
+### Group data by dims
 """
 
 # ╔═╡ b173768c-773f-4130-8029-64e2d2dd743d
 md"""
-# Data manipulation
+## Read dataframe
+"""
+
+# ╔═╡ 35092cc8-7995-4af9-adcf-44738b9b7fc6
+md"""
+Folder to read: $(@bind folder Select(
+(x->x=>x).(readdir(datadir())) |> collect
+))
 """
 
 # ╔═╡ aa1a4f63-8df5-4ea3-a0dc-3ab59a0908f5
-df = collect_results(datadir("polyloop"))
+df = collect_results(datadir(folder));
 
 # ╔═╡ 79f08bdc-94f1-4265-a6d2-0729a537553c
-v, β = getproperty.(df.df, col), df.β
+begin 
+	select!(df, [:β, :dims, :nterm, :data])
+	sort!(df, [:β, :dims, order(:nterm, rev=true)])
+end;
 
-# ╔═╡ 29feb57e-6e0b-43bd-8790-1ed06849df69
-stephist(
-	v, 
-	bins = 100, 
-	label = ["β = $(β[1])" "β = $(β[2])" "β = $(β[3])"],
-	xminorticks = 5,
-)
+# ╔═╡ 8d29521a-f371-4307-bcda-465900aa235d
+@subset!(df, 
+	.!((:β .== 6.46).&(:dims .∈ [[(2,8,8,8), (2,10,10,10), (2,12,12,12)]]).&(:nterm .== 300)));
 
-# ╔═╡ a3b5c6ec-e692-49bf-8d15-e1133d10bea3
-plot(
-	v, 
-	label = ["β = $(β[1])" "β = $(β[2])" "β = $(β[3])"],
-	xlabel = "iteration",
-	xminorticks = 5,
-	yminorticks = 2,
-)
+# ╔═╡ dcf4af44-188d-428d-9d4f-6ae10923c429
+data = 
+@transform(df, 
+	:χᵥ = (x->measurement(mean(x), std(x))).(
+		getproperty.(:data, ^(:χᵥ)) .- 
+		(x->x.^2).(getproperty.(:data, ^(:polyloop)))
+	)
+);
+
+# ╔═╡ 7cb4536b-e3bd-4724-a50f-b82a4c0151ef
+gf = groupby(data, :dims);
 
 # ╔═╡ 66142c02-0d2c-4d16-bf6b-deb0b17f16fa
 md"""
 # Imported packages
 """
 
+# ╔═╡ ba1a70d4-5eae-49ca-9837-966155cf2444
+markers = [:circle, :rect, :star5, :diamond, :hexagon, :cross, :xcross, :utriangle, :dtriangle, :rtriangle, :ltriangle, :pentagon, :heptagon, :octagon, :star4, :star6, :star7, :star8, :vline, :hline, :+, :x]
+
+# ╔═╡ a4622874-cec8-486a-a669-24f6db8db510
+begin
+	p = plot(
+		title = L"\chi / L^3 = \langle\phi\rangle^2 -\langle\phi^2\rangle" * " for (3+1)d Sp(2)", 
+		xlabel = "β = 8/g²", 
+		ylabel = "χ/L³", 
+		xminorticks = 5,
+		yminorticks = 5, 
+		legend = :bottomleft,
+		dpi = 300
+	)
+	for (g, marker) in zip(gf, Iterators.cycle(markers))
+		L = g.dims[begin][2]
+		plot!(g.β, g.χᵥ, 
+			label = "L = $L", 
+			marker = (marker, 3.5), 
+			msc = :auto,
+			markerstrokewidth = 0.2
+		)
+	end
+	wsave(plotsdir("susc", "fig8.png"), p)
+	@info "plot saved"
+	p
+end
+
 # ╔═╡ Cell order:
 # ╟─d5095d25-a5ed-432f-b5dc-8e5f2c68f35f
-# ╟─bb4b3a80-9310-44b1-ad13-2562fc6502bc
-# ╟─29feb57e-6e0b-43bd-8790-1ed06849df69
-# ╟─a3b5c6ec-e692-49bf-8d15-e1133d10bea3
-# ╟─b173768c-773f-4130-8029-64e2d2dd743d
-# ╠═aa1a4f63-8df5-4ea3-a0dc-3ab59a0908f5
+# ╟─a4622874-cec8-486a-a669-24f6db8db510
+# ╟─47761aa2-7222-444b-934d-e0593b2cecbd
+# ╟─4ccc646e-0602-4d6b-a0bf-d412261f9598
 # ╠═79f08bdc-94f1-4265-a6d2-0729a537553c
+# ╟─ad47f215-ba50-45d6-b105-c584e30c85cc
+# ╠═8d29521a-f371-4307-bcda-465900aa235d
+# ╟─e2d78392-435c-4001-8596-f42da7858e07
+# ╠═dcf4af44-188d-428d-9d4f-6ae10923c429
+# ╟─dff10576-2feb-418c-903e-ef11e47f8d74
+# ╠═7cb4536b-e3bd-4724-a50f-b82a4c0151ef
+# ╟─b173768c-773f-4130-8029-64e2d2dd743d
+# ╟─35092cc8-7995-4af9-adcf-44738b9b7fc6
+# ╠═aa1a4f63-8df5-4ea3-a0dc-3ab59a0908f5
 # ╟─66142c02-0d2c-4d16-bf6b-deb0b17f16fa
 # ╠═a7a2033e-19da-45ca-aee7-3821cff5f597
 # ╠═263b0ba3-d191-475d-8183-3babab74affe
 # ╠═0aafddce-0c54-4df5-bf3e-87d684782c87
+# ╠═ba1a70d4-5eae-49ca-9837-966155cf2444
