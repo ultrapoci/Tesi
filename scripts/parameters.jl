2bc8ddb783264e580e1d4cfafe657016f617d0f8
Nicholas Pini <ultrapoci@gmail.com>
2022-05-16 14:29:29 +0200

    Update parameters.jl

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -7,15 +7,17 @@ Base.@kwdef struct ObsParams <: Params
 end
 
 Base.@kwdef struct TermParams <: Params
-	dims = [(2, 8, 8, 8), (2, 10, 10, 10), (2, 12, 12, 12)]
-	β = sort(unique(vcat(6.43:0.01:6.49, 6.456:0.001:6.47)))	
+	# dims = [(2, 8, 8, 8), (2, 10, 10, 10), (2, 12, 12, 12)]
+	# β = sort(unique(vcat(6.43:0.01:6.49, 6.456:0.001:6.47)))	
+	dims = [(2, 8, 8), (2, 10, 10), (2, 12, 12)]
+	β = sort(unique(vcat(6.43:0.01:6.49, 6.456:0.001:6.47)))
 	latticestart = :cold
 	
-	nterm = 10_000
-	nnorm = 100 # after how many cycle to normalize lattice
+	nterm = 300
+	nnorm = 10 # after how many cycle to normalize lattice
 	nover = 3 # how many cycles of overrelaxation to do
 	
-	startobs = 300 # from which iteration to start taking measurements	
+	startobs = 100 # from which iteration to start taking measurements	
 	nobs = 1 # measure observables every nobs cycles
 	
 	observables = (
