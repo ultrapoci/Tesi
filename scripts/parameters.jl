8d33c5ad1b640c0eb908005daab75b752f488293
Nicholas Pini <ultrapoci@gmail.com>
2022-06-24 12:15:26 +0200

    Update parameters.jl

--- scripts/parameters.jl
+++ scripts/parameters.jl
@@ -7,18 +7,13 @@ Base.@kwdef struct ObsParams <: Params
 end
 
 Base.@kwdef struct TermParams <: Params
-	β = sort(unique(vcat(
-		18.0:0.1:21.0
-	)))
-	dims = [(4, L, L) for L in 20:10:80]
+	β = 7.635
+	dims = (20, 16, 16, 16)
 	latticestart = :cold
 	
-	nterm = 15_000
+	nterm = 1_000
 	nnorm = 100 # after how many cycle to normalize lattice
 	nover = 3 # how many cycles of overrelaxation to do
-	
-	startobs = 400 # from which iteration to start taking measurements	
-	nobs = 1 # measure observables every nobs cycles
 	
 	observables = (
 		:plaqs	=> plaquettesum,
@@ -26,10 +21,21 @@ Base.@kwdef struct TermParams <: Params
 		:φ²		=> polyloop2_sum,
 		:φ⁴		=> polyloop4_sum,
 		:modφ	=> polyloopmod_sum,
-		:V		=> (C::ObsConfig; kwargs...) -> length(C.L),
-		:Nₜ		=> (C::ObsConfig; kwargs...) -> first(size((C.L))),
-		:Vₛ		=> (C::ObsConfig; kwargs...) -> spatialvolume(C.L),
+		:R2		=> (C::ObsConfig; kwargs...) -> corr_polyloop(2, C; kwargs...),
+		:R3		=> (C::ObsConfig; kwargs...) -> corr_polyloop(3, C; kwargs...),
+		:R4		=> (C::ObsConfig; kwargs...) -> corr_polyloop(4, C; kwargs...),
+		:R5		=> (C::ObsConfig; kwargs...) -> corr_polyloop(5, C; kwargs...),
+		:R6		=> (C::ObsConfig; kwargs...) -> corr_polyloop(6, C; kwargs...),
+		:R7		=> (C::ObsConfig; kwargs...) -> corr_polyloop(7, C; kwargs...),
+		:R8		=> (C::ObsConfig; kwargs...) -> corr_polyloop(8, C; kwargs...),
+		# :V		=> (C::ObsConfig; kwargs...) -> length(C.L),
+		# :Nₜ		=> (C::ObsConfig; kwargs...) -> first(size((C.L))),
+		# :Vₛ		=> (C::ObsConfig; kwargs...) -> spatialvolume(C.L),
 	)
+	
+	# startobs = 400 # from which iteration to start taking measurements	
+	# nobs = 1 # measure observables every nobs cycles
+	
 	#= observables = (
 		:avg_plaq	=> averageplaquette,
 		:φ			=> expval_polyloop,
