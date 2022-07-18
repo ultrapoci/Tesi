5317bdaad68534677a59e14573d23fc5b7126e2b
Nicholas Pini <ultrapoci@gmail.com>
2022-07-18 14:01:29 +0200

    Update analysis.jl

--- notebooks/analysis.jl
+++ notebooks/analysis.jl
@@ -37,10 +37,10 @@ md"""
 """
 
 # ╔═╡ 07a9828f-3de7-44e7-af62-4aa9ccac9cda
-datafolder = datadir("rust", "nt=2")
+datafolder = datadir("rust", "nt=4-9")
 
 # ╔═╡ 5d983982-21c0-4c83-b14b-a8e9fd6c9683
-skip_offset = 400
+skip_offset = 500
 
 # ╔═╡ 37178a96-1173-43c6-ba4a-9e5e8ee4ad32
 binsize = 20
@@ -65,9 +65,9 @@ md"""
 
 # ╔═╡ bb7314f0-80c7-4e0d-89e9-2302824e40aa
 md"""
-Select L:
+All Ls: $(@bind all_ls CheckBox(default=true))
 
-All: $(@bind all_ls CheckBox(default=true))
+All Nts: $(@bind all_nts CheckBox(default=true))
 """
 
 # ╔═╡ f6d1d0c2-6ce6-4ff8-8ac7-3de03545ce16
@@ -88,19 +88,12 @@ md"""
 # ╔═╡ 459314f0-efed-4231-8a68-fca7bd0da737
 filelist = readdir(datafolder, join=true)
 
-# ╔═╡ 8cdb6d2d-2248-472f-b883-64c33a940187
-function readinfo(f)
-	d = CSV.read(f, DataFrame, limit = 1)[1, :]
-	Dict(Symbol.(names(d)) .=> values(d))
-end
-
-# ╔═╡ 74653267-7842-469a-a9d1-c5cd693cdec6
-readdata(f) = CSV.read(f, DataFrame, header = 3)
-
 # ╔═╡ 35a679fe-989b-4b27-950d-afd56840ae88
 function add_measurements!(dict, offset = 0)
 	data = last(dict[:data], nrow(dict[:data]) - offset)
 	mapcols!(col -> mean.(binsamples(col, binsize)), data)
+
+	# data = dict[:data]
 	
 	V = dict[:v] # volume
 	Vs = dict[:v_s] #spatial volume
@@ -122,10 +115,10 @@ function add_measurements!(dict, offset = 0)
 	dict[:S²] = jackknife(S², data.plaquettes)
 	dict[:gᵣ] = jackknife(gᵣ, data.polyloops2, data.polyloops4)
 	dict[:Cᵥ] = jackknife(Cᵥ, S².(data.plaquettes), S.(data.plaquettes))
-	#dict[:Cᵥ] = (mean(S².(data.plaquettes)) - mean(S.(data.plaquettes))) / V
-	dict[:susc] = mean(χᵥ.(data.polyloops2, data.polyloops_mod))
-	dict[:susc2] = χᵥ(mean(data.polyloops2), mean(data.polyloops_mod))
 end
+
+# ╔═╡ e5f3645f-1740-4163-9220-7d7189cf1aaa
+getkeys(df, k) = getproperty.(keys(df), k)
 
 # ╔═╡ 37679ab2-f768-47ec-927d-d359d6fd036c
 begin
@@ -149,15 +142,30 @@ begin
 end;
 
 # ╔═╡ f4b0c054-7500-4fed-8fba-ab9b481d410d
-dfs = groupby(df, :l);
+dfs = groupby(df, [:nt, :l]);
 
 # ╔═╡ 30ca6749-22b3-4134-88eb-48ccc2b79f0d
-if !all_ls
-	md"""$(@bind l_list MultiSelect(first.(keys(dfs)), default = first.(keys(dfs))))"""
+begin
+	L_values = sort(unique(getkeys(dfs, :l)))
+	Nt_values = sort(unique(getkeys(dfs, :nt)))
+	L_md = @bind l_list MultiSelect(L_values, default = L_values)
+	Nt_md = @bind nt_list MultiSelect(Nt_values, default = Nt_values)
+	if !all_ls && !all_nts
+		md"""L =
+		$L_md
+
+		Nt =
+		$Nt_md
+		"""
+	elseif !all_ls
+		md"""L = $L_md"""
+	elseif !all_nts
+		md"""Nt = $Nt_md"""
+	end
 end
 
 # ╔═╡ 1d135b91-8121-43e3-86a9-e583a947ef32
-obsnames = [:avg_plaq, :χ, :χᵥ, :S, :S², :gᵣ, :Cᵥ, :susc, :susc2]
+obsnames = [:avg_plaq, :χ, :χᵥ, :S, :S², :gᵣ, :Cᵥ]
 
 # ╔═╡ e5fd0de8-8222-4e0d-a555-6cb078960e77
 md"""
@@ -199,11 +207,20 @@ begin
 	Ls = try
 		l_list
 	catch
-		first.(keys(dfs))
+		getkeys(dfs, :l)
+	end
+
+	Nts = try
+		nt_list
+	catch
+		getkeys(dfs, :nt)
 	end
 	
-	for (df, L, marker) in zip(dfs, first.(keys(dfs)), Iterators.cycle(markers))
+	for ((k, df), marker) in zip(pairs(dfs), Iterators.cycle(markers))
+		L = k.l
+		Nt = k.nt
 		L ∉ Ls && continue
+		Nt ∉ Nts && continue
 			
 		v = use_error_bars ? getproperty(df, plot_col) : Measurements.value.(getproperty(df, plot_col))
 		
@@ -214,7 +231,7 @@ begin
 		end
 		
 		plot!(p, df.beta[inds], v[inds], 
-			label = "L = $L", 
+			label = "Nₜ=$Nt, L=$L", 
 			marker = (marker, 3.5), 
 			msc = :auto,
 			markerstrokewidth = 0.5
@@ -257,26 +274,6 @@ begin
 	first_trigger[] = true
 end;
 
-# ╔═╡ 2b6e4e88-db1e-46d4-b9b8-29a0b10cf615
-md"""
-# Test
-"""
-
-# ╔═╡ 4a430497-5767-4218-94fa-0cbdfac448d5
-filename = filelist[1]
-
-# ╔═╡ 09b55a16-789b-4bc0-be4d-712f933e7017
-info = readinfo(filename)
-
-# ╔═╡ 0ba2e0fb-1a69-44f0-967e-3d2ed4c423f4
-test = readdata(filename)
-
-# ╔═╡ 763db54e-2694-4e05-8574-9d6918fb4327
-plaqs = test.plaquettes
-
-# ╔═╡ a539170d-6cba-4e8c-9247-8bead91b42f5
-mean(plaqs) / (3 * 8^3)
-
 # ╔═╡ Cell order:
 # ╟─9d70e646-4dfb-4fed-9b00-383fdf3923bb
 # ╠═07a9828f-3de7-44e7-af62-4aa9ccac9cda
@@ -297,9 +294,8 @@ mean(plaqs) / (3 * 8^3)
 # ╠═f3ef5f6d-5646-4daa-95ad-6b84d14bf17e
 # ╟─1fc2eae0-b2ce-4839-b2b7-658d2970352d
 # ╟─459314f0-efed-4231-8a68-fca7bd0da737
-# ╠═8cdb6d2d-2248-472f-b883-64c33a940187
-# ╠═74653267-7842-469a-a9d1-c5cd693cdec6
 # ╠═35a679fe-989b-4b27-950d-afd56840ae88
+# ╠═e5f3645f-1740-4163-9220-7d7189cf1aaa
 # ╠═37679ab2-f768-47ec-927d-d359d6fd036c
 # ╠═31bc8726-638b-402e-8ff9-34920a452d31
 # ╠═f4b0c054-7500-4fed-8fba-ab9b481d410d
@@ -315,9 +311,3 @@ mean(plaqs) / (3 * 8^3)
 # ╠═b573b0a5-a23a-47e0-9b6b-e69f49563e89
 # ╠═19ffdd0b-1b22-4ee7-bc52-5042b9058280
 # ╠═0fd26021-cc9f-4383-bf32-43b11e564f0f
-# ╟─2b6e4e88-db1e-46d4-b9b8-29a0b10cf615
-# ╠═4a430497-5767-4218-94fa-0cbdfac448d5
-# ╠═09b55a16-789b-4bc0-be4d-712f933e7017
-# ╠═0ba2e0fb-1a69-44f0-967e-3d2ed4c423f4
-# ╠═763db54e-2694-4e05-8574-9d6918fb4327
-# ╠═a539170d-6cba-4e8c-9247-8bead91b42f5
